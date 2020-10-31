7142b411701e9877305120a59c9c5d77cee546c2
A5rocks <40616000+A5rocks@users.noreply.github.com>
2020-10-31 10:35:36 +0900

    Fixes my lack of abilities on IDEs

--- src/main/java/Oreo/Trie.java
+++ src/main/java/Oreo/Trie.java
@@ -4,7 +4,6 @@ import java.util.Map;
 import java.util.HashMap;
 import java.util.function.Consumer;
 
-import com.koloboke.collect.impl.hash.Hash;
 import com.mewna.catnip.entity.message.Message;
 
 public class Trie {
